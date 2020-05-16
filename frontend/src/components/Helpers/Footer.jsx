ecec90795ea2191ac18c051e0e48bc7633269f2d
Jose A. Alvarado <megaalpha200@gmail.com>
2020-05-16 16:37:40 -0500

    Updated files in order to set up HTTPS SSL

--- frontend/src/components/Helpers/Footer.jsx
+++ frontend/src/components/Helpers/Footer.jsx
@@ -3,7 +3,7 @@ import 'assets/Helpers/css/footerStyle.css';
 
 const Footer = () => (
     <div id="footer-container">
-        <div class="footer-item" align="center">
+        <div className="footer-item" align="center">
             <br/>
             <p id="copyright">&copy; #HexiPi 2020<br />All Rights Reserved</p>
         </div>
