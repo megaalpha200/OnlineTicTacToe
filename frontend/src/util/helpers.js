b94d58cfe56e209fc630b2107ba5fea002d510d9
Jose A. Alvarado <megaalpha200@gmail.com>
2020-07-16 18:21:06 -0500

    Enabled website_frontend and certbot in docker-compose.yml

--- frontend/src/util/helpers.js
+++ frontend/src/util/helpers.js
@@ -1,4 +1,4 @@
-import psl from 'psl';
+//import psl from 'psl';
 
 export const parseError = err => {
     if (err.isJoi) return err.details[0];
@@ -6,4 +6,5 @@ export const parseError = err => {
 };
 
 export const currentLocation = `${window.location.origin}`;
-export const backendEndpoint = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? `${window.location.protocol}//${window.location.hostname}:8524` : `${window.location.protocol}//api.${psl.parse(window.location.hostname).domain}`;
+export const backendEndpoint = `${window.location.protocol}//${window.location.hostname}:8524`
+//export const backendEndpoint = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? `${window.location.protocol}//${window.location.hostname}:8524` : `${window.location.protocol}//api.${psl.parse(window.location.hostname).domain}`;
